0fc6085e26f678434e9273ae3833f35363a7c2b5
Victor CS <cifusuil.victor@gmail.com>
2024-05-11 22:16:39 -0400

    First commit.

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Jammming: Spotify API React Project
         </p>
         <a
           className="App-link"
